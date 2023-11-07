b19a8b416b6d526a77c8b9e0b3866d23a2eb5ed6
Cadin Londono <cadinsl@gmail.com>
2023-11-06 19:40:35 -0500

    Added wwise footsteps events

--- Assets/Scripts/Adventure/Player/CharacterMovement.cs
+++ Assets/Scripts/Adventure/Player/CharacterMovement.cs
@@ -8,28 +8,39 @@ public class CharacterMovement : MonoBehaviour
 
     private CharacterController characterController;
     private AdventurePlayerAnimation adventurePlayerAnimation;
+    private PlayerAdventureInput playerAdventureInput;
 
     // Start is called before the first frame update
     void Start()
     {
         characterController = gameObject.GetComponent<CharacterController>();
         adventurePlayerAnimation = gameObject.GetComponent<AdventurePlayerAnimation>();
+        playerAdventureInput = gameObject.GetComponent<PlayerAdventureInput>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        move = Vector3.ClampMagnitude(move, 1);
-        if(adventurePlayerAnimation != null)
+        if (playerAdventureInput.isInputEnabled())
         {
-            adventurePlayerAnimation.SetSpeed(move.magnitude);
-        }
-        characterController.SimpleMove(move * movementSpeed);
+            Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            move = Vector3.ClampMagnitude(move, 1);
+            if (adventurePlayerAnimation != null)
+            {
+                adventurePlayerAnimation.SetSpeed(move.magnitude);
+            }
+            characterController.SimpleMove(move * movementSpeed);
 
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
+            if (move != Vector3.zero)
+            {
+                gameObject.transform.forward = move;
+            }
         }
+    }
+
+    public void StopMovement()
+    {
+        adventurePlayerAnimation.SetSpeed(0);
+
     }
 }
